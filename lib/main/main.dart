ddea57ad3673a3d08fdb42137a23cd0449e6ab59
Aristocrat <crataristo4@gmail.com>
2021-07-20 22:29:02 +0000

    main and entry point added

---
+++ lib/main/main.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:gelic_bakes/provider/auth_provider.dart';
+import 'package:gelic_bakes/ui/auth/config.dart';
+import 'package:gelic_bakes/ui/onboarding/onboarding_page.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'route_generator.dart';
+
+int? onboardingPrefs;
+final GlobalKey<State> loadingKey = new GlobalKey<State>();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  MobileAds.instance.initialize();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  onboardingPrefs = prefs.getInt("onboarding");
+  await prefs.setInt("onboarding", 1);
+  runApp(EntryPoint());
+}
+
+class EntryPoint extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        //authentication
+        ChangeNotifierProvider.value(value: AuthProvider()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          primaryColor: Colors.pink,
+        ),
+        initialRoute: onboardingPrefs == 0 || onboardingPrefs == null
+            ? OnboardingPage
+                .routeName //shows when app data is cleared or newly installed
+            : ConfigurationPage.routeName,
+        onGenerateRoute: RouteGenerator.generateRoute,
+      ),
+    );
+  }
+}
