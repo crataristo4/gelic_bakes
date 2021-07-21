d337a96f3159c85b1a4694d944ba22d23107e6a2
Aristocrat <crataristo4@gmail.com>
2021-07-21 08:54:55 +0000

    custom clipper added

--- lib/ui/sidebar/sidebar.dart
+++ lib/ui/sidebar/sidebar.dart
@@ -116,15 +116,18 @@ class _SidebarItemState extends State<SidebarItem>
                     onTap: () {
                       triggerSideBar();
                     },
-                    child: Container(
-                      width: 35,
-                      height: 110,
-                      color: Colors.pink,
-                      alignment: Alignment.centerLeft,
-                      child: AnimatedIcon(
-                        progress: _animationController!.view,
-                        icon: AnimatedIcons.menu_close,
-                        color: Colors.white,
+                    child: ClipPath(
+                      clipper: CustomMenuClipper(),
+                      child: Container(
+                        width: 35,
+                        height: 110,
+                        color: Colors.pink,
+                        alignment: Alignment.centerLeft,
+                        child: AnimatedIcon(
+                          progress: _animationController!.view,
+                          icon: AnimatedIcons.menu_close,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
@@ -133,5 +136,31 @@ class _SidebarItemState extends State<SidebarItem>
             ),
           );
         });
+  }
+}
+
+class CustomMenuClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Paint paint = Paint();
+    paint.color = Colors.white;
+
+    final width = size.width;
+    final height = size.height;
+
+    Path path = Path();
+    path.moveTo(0, 0);
+    path.quadraticBezierTo(0, 8, 10, 16);
+    path.quadraticBezierTo(width - 1, height / 2 - 20, width, height / 2);
+    path.quadraticBezierTo(width + 1, height / 2 + 20, 10, height - 16);
+    path.quadraticBezierTo(0, height - 8, 0, height);
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
