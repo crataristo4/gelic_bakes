5c9a3f7a9f30da20e504d6a43f9ed58b01db0a19
Aristocrat <crataristo4@gmail.com>
2021-08-04 14:16:15 +0000

    delete unused class

--- lib/ui/pages/home_widgets/special_offer/view_special_offer.dart
+++ lib/ui/pages/home_widgets/special_offer/view_special_offer.dart
@@ -11,6 +11,7 @@ import 'package:gelic_bakes/models/promotion.dart';
 import 'package:gelic_bakes/provider/promo_provider.dart';
 import 'package:gelic_bakes/service/admob_service.dart';
 import 'package:gelic_bakes/ui/bottomsheets/pre_order.dart';
+import 'package:gelic_bakes/ui/widgets/showImage.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 import 'package:shimmer/shimmer.dart';
@@ -419,36 +420,4 @@ class _ViewSpecialOffersState extends State<ViewSpecialOffers>
   }
 }
 
-//Display full image
-class ShowImageScreen extends StatelessWidget {
-  final image;
 
-  const ShowImageScreen({Key? key, this.image}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Navigator.pop(context),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            //  color: const Color(0xff7c94b6),
-            color: Colors.black45,
-            image: DecorationImage(
-              fit: BoxFit.fitWidth,
-              colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.2), BlendMode.colorDodge),
-              image: CachedNetworkImageProvider(
-                image!,
-              ),
-            ),
-          ),
-          padding: EdgeInsets.symmetric(horizontal: tenDp),
-        ),
-      ),
-    );
-  }
-}
