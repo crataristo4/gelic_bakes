3bb62035a6ab908f0f303f7c67f981637120d4ed
Aristocrat <crataristo4@gmail.com>
2021-08-04 14:14:09 +0000

    delete unused class

--- lib/ui/pages/home_widgets/category/item_category.dart
+++ lib/ui/pages/home_widgets/category/item_category.dart
@@ -10,6 +10,7 @@ import 'package:gelic_bakes/ui/bottomsheets/pre_order.dart';
 import 'package:gelic_bakes/ui/pages/home_widgets/category/details_psge.dart';
 import 'package:gelic_bakes/ui/pages/orders.dart';
 import 'package:gelic_bakes/ui/widgets/loading.dart';
+import 'package:gelic_bakes/ui/widgets/showImage.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class CategoryItems extends StatefulWidget {
@@ -420,36 +421,4 @@ class _CategoryItemsState extends State<CategoryItems> {
   }
 }
 
-//Display full image
-class ShowImageScreen extends StatelessWidget {
-  final image;
 
-  const ShowImageScreen({Key? key, this.image}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Navigator.pop(context),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            //  color: const Color(0xff7c94b6),
-            color: Colors.black45,
-            image: DecorationImage(
-              fit: BoxFit.fitWidth,
-              colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.2), BlendMode.colorDodge),
-              image: CachedNetworkImageProvider(
-                image!,
-              ),
-            ),
-          ),
-          padding: EdgeInsets.symmetric(horizontal: tenDp),
-        ),
-      ),
-    );
-  }
-}
